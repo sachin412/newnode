a0c5cb3c02f1ff27108bc2cabe395ca9f3ac6a0e
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-02 21:33:38 +0530

    adding server url

--- sonar-project.js
+++ sonar-project.js
@@ -1,5 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
+       serverUrl: 'http://localhost:9000',   
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'src/**' // Entry point of your code
