6ce44288a9f9a86cfd8e1606736fbbdc23e415d2
vivek.rajyaguru <vivek.rajyaguru@volansys.com>
2019-09-03 14:31:47 +0530

    change done from localcode

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,8 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://localhost:9000',   
+       serverUrl: 'http://192.168.3.244:9000',   
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'src/**' // Entry point of your code
-       }
+       }  
      }, () => {});
