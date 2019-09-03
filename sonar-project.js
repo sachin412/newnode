f040403aa1be20f3da5b3b0533a8a1d6c1625a2a
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-03 15:59:43 +0530

    solving eslint errors

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,8 @@
 const sonarqubeScanner = require('sonarqube-scanner')
 sonarqubeScanner({
         serverUrl: 'http://192.168.3.244:9000',   
-           options : {
-                   'sonar.sources': '.',
-                   'sonar.inclusions' : 'src/**' // Entry point of your code
-       }  
-     }, () => {});
+        options : {
+                'sonar.sources': '.',
+                'sonar.inclusions' : 'src/**' // Entry point of your code
+        }  
+}, () => {});
