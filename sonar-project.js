dd59169b578af3190c23449206192a332a0c59c5
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-03 18:35:52 +0530

    correcting code space

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,8 @@
 const sonarqubeScanner = require('sonarqube-scanner')
 sonarqubeScanner({
-  serverUrl: 'http://192.168.3.244:9000',   
-  options : {
-    'sonar.sources': '.',
-    'sonar.inclusions' : 'services/**' // Entry point of your code
- }  
+    serverUrl: 'http://192.168.3.244:9000',   
+    options : {
+        'sonar.sources': '.',
+        'sonar.inclusions' : 'services/**' // Entry point of your code
+    }  
 }, () => {})
