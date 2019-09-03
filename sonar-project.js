f7f33f878f710e74d792ce14fa7a821aaed7edb6
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-03 16:20:29 +0530

    putting name instead of ip

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
-        serverUrl: 'http://192.168.3.244:9000',   
+        serverUrl: 'http://sachin:9000',   
         options : {
                 'sonar.sources': '.',
                 'sonar.inclusions' : 'src/**' // Entry point of your code
