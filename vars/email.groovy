92315b3474ecb71c626db24f4de54cf11dc15654
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-10 15:59:38 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,8 +1,4 @@
 #!/usr/bin/env groovy
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.net.URL
-import java.net.URLConnection
+ 
 
 mail bcc: '', body: 'please check your jobs console output', cc: '', from: '', replyTo: '', subject: 'job status failed', to: 'sachin.pavar@volansys.com'
