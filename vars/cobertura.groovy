7563758119dfbc2e56b8e202bb23f1ed96811240
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-10 16:56:30 +0530

    Update cobertura.groovy

--- vars/cobertura.groovy
+++ vars/cobertura.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String buildStatus = 'STARTED'){
- cobertura autoUpdateHealth: true, autoUpdateStability: false, coberturaReportFile: 'reports/cobertura-coverage.xml', conditionalCoverageTargets: '70, 0, 0', enableNewApi: true, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 5, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+ 
+cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: 'reports/cobertura-coverage.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, zoomCoverageChart: false 
+
 }
