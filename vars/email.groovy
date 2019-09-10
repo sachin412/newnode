fc5850cb64eb918e28529d523fb9c395a446a139
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-10 16:09:59 +0530

    defining call

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy
- 
+
+def call(String buildStatus = 'STARTED'){
 
 mail bcc: '', body: 'please check your jobs console output', cc: '', from: '', replyTo: '', subject: 'job status failed', to: 'sachin.pavar@volansys.com'
+
+}
