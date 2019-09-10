e44a292fe096b8ff922078742912e6c03c0ad4b2
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-10 16:37:18 +0530

    adding cobertura command

---
+++ vars/cobertura.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def call(String coberturaStatus = 'STARTED'){
+cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: 'reports/cobertura-coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+
+}
