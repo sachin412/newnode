0e07899cf3566f318c3c87425de14509a97ad0c3
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-10 19:05:27 +0530

    add sonar command in groovy

--- vars/_sonar.groovy
+++ vars/_sonar.groovy
@@ -2,8 +2,8 @@
 
 def call(String buildStatus = 'STARTED'){
  
-  withSonarQubeEnv(credentialsId: '8dbd6980-2c82-4528-baef-a4b58f2c22cd') {
-    node sonar-project.js
-}
+withSonarQubeEnv('sonarqube') {
+                sh 'node sonar-project.js'
+                   }
  
 }
