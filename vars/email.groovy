350c77283b6315221e7466a5af25786bc55b0f42
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-11 11:44:04 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -2,6 +2,6 @@
 
 def call(String buildStatus = 'STARTED'){
 
-mail bcc: '', body: 'please check your jobs console output', cc: '', from: '', replyTo: '', subject: 'job status failed', to: 'sachin.pavar@volansys.com'
+  mail bcc: '', body: 'please check your jobs console output "${BUILD_LOG}"', cc: '', from: '', replyTo: '', subject: 'job status failed', to: 'sachin.pavar@volansys.com'
 
 }
